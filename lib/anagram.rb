9a9d87f585d2349b7fde74f59cdafd54579955d6
theo <teddyhoover@gmail.com>
2020-12-15 12:53:41 -0600

    commit tree ab6e76acea6d7dcab4fcbe10a6c9d2e80a1f2daf

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+    
+    def initialize(word)
+        @word = word
+    end
+
+    def match(possible_matches_array)
+        matches_array = []
+
+        letter_array = word.split("").sort
+        
+        possible_matches_array.each do |posi_match|
+          posi_word_array = posi_match.split("").sort
+          if posi_word_array == letter_array
+              matches_array << posi_match 
+          end
+
+        end
+
+        matches_array
+    end
+end
